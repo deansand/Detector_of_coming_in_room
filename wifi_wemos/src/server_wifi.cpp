f98ab690b1780f13389dc2ce25bc941b54bd6fd8
deansand <danylopetrushenko@gmail.com>
2025-03-17 09:11:38 +0200

    Update HTML structure and button functionality for ESP LED Control interface

--- wifi_wemos/src/server_wifi.cpp
+++ wifi_wemos/src/server_wifi.cpp
@@ -57,25 +57,20 @@ void setup(){
       <!DOCTYPE html>
       <html>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="data:,">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="icon" href="data:,">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
       </head>
       <body>
-        <div class="container">
-          <div class="row">
-            <h1>ESP Color Picker</h1>
-          </div>
-          <a class="btn btn-primary btn-lg" href="#" id="change_color" role="button">Change Color</a>
-          <input class="jscolor {onFineChange:'update(this)'}" id="rgb">
+      <div class="container">
+        <div class="row">
+        <h1>ESP LED Control</h1>
         </div>
-        <script>
-          function update(picker) {
-            document.getElementById('rgb').innerHTML = Math.round(picker.rgb[0]) + ', ' +  Math.round(picker.rgb[1]) + ', ' + Math.round(picker.rgb[2]);
-            document.getElementById("change_color").href = "/led?r=" + Math.round(picker.rgb[0]) + "&g=" +  Math.round(picker.rgb[1]) + "&b=" + Math.round(picker.rgb[2]);
-          }
-        </script>
+        <div class="row">
+        <a class="btn btn-light btn-lg" href="/led?r=255&g=255&b=255" role="button">White</a>
+        <a class="btn btn-secondary btn-lg" href="/led?r=238&g=130&b=238" role="button">Violet</a>
+        </div>
+      </div>
       </body>
       </html>
     )rawliteral");
